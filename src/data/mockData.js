b055d437883612e103f8e5b4837e16f9231c755a
Damian Canton <damiancanton278@gmail.com>
2026-01-12 01:09:25 -0300

    fix: tareas del lunes 12 de enero

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -8,62 +8,6 @@ export const mockTasks = {
   [today]: [
     {
       id: "1",
-      time: "09:00-11:00",
-      title: "Diseñar interfaz",
-      category: "work",
-      done: true,
-      notes: "Usar Material Design 3",
-      isHabit: false,
-    },
-    {
-      id: "2",
-      time: "11:00-12:00",
-      title: "Daily Meeting",
-      category: "work",
-      done: true,
-      notes: "Revisar métricas del equipo",
-      isHabit: false,
-    },
-    {
-      id: "3",
-      time: "13:00-14:00",
-      title: "Almuerzo",
-      category: "personal",
-      done: true,
-      isHabit: false,
-    },
-    {
-      id: "4",
-      time: "15:00-17:00",
-      title: "Implementar Componentes",
-      category: "work",
-      done: false,
-      notes: "Card, Button, Modal",
-      isHabit: false,
-    },
-    {
-      id: "5",
-      time: "18:00-19:00",
-      title: "Gimnasio",
-      category: "sport",
-      done: false,
-      isHabit: true,
-      habitFrequency: "daily",
-    },
-    {
-      id: "6",
-      time: "07:00-08:00",
-      title: "Meditación",
-      category: "personal",
-      done: false,
-      notes: "使用Headspace应用",
-      isHabit: true,
-      habitFrequency: "weekdays",
-    },
-  ],
-  [tomorrow]: [
-    {
-      id: "7",
       time: "09:30-11:00",
       title: "Arreglar techo deck",
       category: "personal",
@@ -71,7 +15,7 @@ export const mockTasks = {
       isHabit: false,
     },
     {
-      id: "8",
+      id: "2",
       time: "11:00-13:30",
       title: "Avanzar tp final Angular",
       category: "study",
@@ -79,7 +23,7 @@ export const mockTasks = {
       isHabit: false,
     },
     {
-      id: "9",
+      id: "3",
       time: "14:00-17:00",
       title: "Avanzar tp final Angular",
       category: "study",
@@ -87,7 +31,7 @@ export const mockTasks = {
       isHabit: false,
     },
     {
-      id: "10",
+      id: "4",
       time: "17:00-17:30",
       title: "Caminar",
       category: "sport",
@@ -96,6 +40,7 @@ export const mockTasks = {
       habitFrequency: "daily",
     },
   ],
+  [tomorrow]: [],
   [yesterday]: [
     {
       id: "9",
